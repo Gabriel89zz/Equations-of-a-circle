835d2f5fbe390ba994290e05c6d8743ee4cb02c0
Gabriel Torres <elcomparosh97@gmail.com>
2024-02-23 18:57:24 -0600

    Agregar archivos de proyecto.

---
+++ Equations of a circle/Circle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Equations_of_a_circle
+{
+    internal class Circle
+    {
+		private double radio;
+
+		public double Radio
+		{
+			get { return radio; }
+			set { radio = value; }
+		}
+
+		private double h;
+
+		public double H
+		{
+			get { return h; }
+			set { h = value; }
+		}
+
+		private double k;
+
+		public double K
+		{
+			get { return k; }
+			set { k = value; }
+		}
+
+		public Circle()
+		{
+				radio=0; h=0; k=0;
+		}
+
+        public Circle(double radio,double h,double k)
+        {
+			this.k = k;
+			this.radio = radio;
+			this.h = h;
+        }
+
+        //Receive-only method
+        public void DisplayCircleInfo(Circle c)
+        {
+            Console.WriteLine("The equation of the circle with center: (" + c.H + "," + c.K + ")" + " and radio: " + c.Radio + "\nis: ");
+        }
+
+        //Method that only returns
+        public string GetEquationOfTheCircle()
+		{
+			return "x²+y²= " +Math.Pow(radio,2);
+		}
+
+        //Method that returns and receives
+        public string GetEquationOfTheCircle(double radio,double h, double k)
+        {
+            string signo = ((-1*h) >= 0) ? "+" : "-";
+            string signo2 = ((-1*k) >= 0) ? "+" : "-";
+            return "(x" + signo + Math.Abs(h) + ")²+" + "(y" + signo2 + Math.Abs(k) + ")²= " + Math.Pow(radio, 2);
+        }
+
+        //Overloaded method
+        public string GetEquationOfTheCircle(Circle c)
+        {
+			double D = (-2)*c.h;
+            string signo = (D >= 0) ? "+" : "-";
+            double E = (-2)*c.k;
+            string signo2 = (E >= 0) ? "+" : "-";
+            double F = (Math.Pow(c.h,2))+ (Math.Pow(c.k, 2)-(Math.Pow(c.radio, 2)));
+            return "x²+y²" + signo + Math.Abs(D) + "x" + signo2 + Math.Abs(E) + "y+"+F+"=0";
+        }
+
+        ~Circle()
+        {
+             Console.WriteLine("Object is being finalized. Cleaning up resources.");
+        }
+    }
+}
---
+++ Equations of a circle/Program.cs
@@ -0,0 +1,16 @@
+﻿using Equations_of_a_circle;
+
+Circle c1 = new Circle();
+c1.Radio = 2;
+c1.H = 3;
+c1.K = 4;
+
+
+c1.DisplayCircleInfo(c1);
+Console.WriteLine(c1.GetEquationOfTheCircle(c1.Radio, c1.H, c1.K));
+Console.WriteLine(c1.GetEquationOfTheCircle(c1)+"\n");
+
+Circle c2 = new Circle();
+c2.Radio = 8;
+c2.DisplayCircleInfo(c2);
+Console.WriteLine(c2.GetEquationOfTheCircle());
